60b2a3b9e6997d104d2a2d1a4c043f525f40a518
YASHWANTHKUMARD <168270104+YASHWANTHKUMARD@users.noreply.github.com>
2025-12-28 13:28:12 +0530

    Add reference database for Bengaluru topics
    Contains Reference_DB

---
+++ bengaluru_references.py
@@ -0,0 +1,132 @@
+%%writefile bengaluru_references.py
+REFERENCE_DB = {
+
+    "history": {
+        "websites": [
+            {
+                "title": "History of Bengaluru – Wikipedia",
+                "urls": [
+                    "https://en.wikipedia.org/wiki/History_of_Bangalore",
+                    "https://bengaluruurban.nic.in/en/history/",
+                    "https://www.britannica.com/place/Bangalore-India"
+                ]
+            }
+        ],
+        "youtube": [
+            {"title": "History of Bengaluru City 1", "url": "https://youtu.be/8jQ43L4-_TA?si=Kn_HBDm5Ra67Qhgz"},
+            {"title": "History of Bengaluru City 2", "url": "https://youtu.be/tzRtT4x3THU?si=pV5_73czfiJ_6y3H"},
+            {"title": "History of Bengaluru City 3", "url": "https://youtu.be/yPaPOXCAs_k?si=pSecnZTq-966AHSV"}
+        ],
+        "images": [
+            "https://static.theprint.in/wp-content/uploads/2024/07/A-crossroads-of-trade-1024x576.jpg",
+            "https://blogs.revv.co.in/blogs/wp-content/uploads/2020/01/Bangalore-Palace.jpg",
+            "https://i.pinimg.com/736x/68/8e/fe/688efeec107327178e826b1ec481f1ce.jpg"
+        ]
+    },
+
+    "geography": {
+        "websites": [
+            {
+                "title": "Geography of Bengaluru – Wikipedia",
+                "urls": [
+                    "https://en.wikipedia.org/wiki/Bangalore#Geography",
+                    "https://ga.bujournals.com/articles/mapping-and-analysis-of-topography-of-bangalore-metropolitan-region",
+                    "https://www.bengaluruonline.in/guide/geography-of-bangalore"
+                ]
+            }
+        ],
+        "youtube": [
+            {"title": "Geography of Bengaluru 1", "url":"https://youtu.be/7JD80-9y9s4?si=AmpsyCOl6BbHYFIV"},
+            {"title": "Geography of Bengaluru 2", "url":"https://youtu.be/kJkLdmU1grA?si=Q6Lz6NIyEGLDC03G"},
+            {"title": "Geography of Bengaluru 3", "url":"https://youtu.be/dLuF4uNETIE?si=tnTqqualHceNDZ4z"}
+        ],
+        "images": [
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Nandi_hillside.jpg/250px-Nandi_hillside.jpg",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZpaytqIA_8xRqh6luIxODIwi2jqLwMpekzg&s",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOJoXgSKFbDoKMNnnF1GvA56p89r-jAjPSKA&s"
+        ]
+    },
+
+    "culture": {
+        "websites": [
+            {
+                "title": "Culture of Bengaluru – Wikipedia",
+                "urls": [
+                    "https://en.wikipedia.org/wiki/Culture_of_Bangalore",
+                    "https://worldcitiescultureforum.com/city/bengaluru/",
+                    "https://bengaluruurban.nic.in/en/kannadaculture/"
+                ]
+            }
+        ],
+        "youtube": [
+            {"title": "Culture of Bengaluru 1", "url":"https://youtu.be/cjhlcIOgUy4?si=-JByaXOp6sKJwIR4"},
+            {"title": "Culture of Bengaluru 2", "url":"https://youtu.be/A5OExmGQ8Tc?si=2B2NKgMmC7fFx9Lt"},
+            {"title": "Culture of Bengaluru 3", "url":"https://youtu.be/GzEdigCvrNs?si=VzwyFxV4565uRKZG"}
+        ],
+        "images": [
+            "https://media.tacdn.com/media/attractions-splice-spp-360x240/07/72/4b/05.jpg",
+            "https://media-cdn.tripadvisor.com/media/attractions-splice-spp-720x480/0b/5a/64/ef.jpg",
+            "https://staybloom.com/content/1702447423892-Mobile.jpg"
+        ]
+    },
+
+    "economy": {
+        "websites": [
+            {
+                "title": "Economy of Bengaluru – Wikipedia",
+                "urls": [
+                    "https://en.wikipedia.org/wiki/Economy_of_Bangalore",
+                    "https://www.india-briefing.com/news/bengaluru-karnataka-india-investment-brief-industries-infrastructure-6371.html/",
+                    "https://www.statista.com/topics/11123/indian-megacity-of-bengaluru/?srsltid=AfmBOopB2DA_8KllpFhM3FsUOsjCDneaBqXT2yq4YWTrPkY1a1WX5Blw"
+                ]
+            }
+        ],
+        "youtube": [
+            {"title": "Economy of Bengaluru 1", "url":"https://youtu.be/YBeDcrr38Bc?si=jyyUeMt0l4sPxe6Y"},
+            {"title": "Economy of Bengaluru 2", "url":"https://youtu.be/cjhlcIOgUy4?si=ZJxLI5EvTCIofrx6"},
+            {"title": "Economy of Bengaluru 3", "url":"https://youtu.be/bR3jVzQ6MCo?si=gRQgXtF7t-VtEo74"},
+            {"title": "Economy of Bengaluru 4", "url":"https://youtu.be/PzQHR7XD0DQ?si=T887BLqqduyz-bUD"}
+        ],
+        "images": [
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNbfkYLoV_OHgUS7-qCdcHZkT3qeHRMQ1HKA&s",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1IQu6HWk9FczxMJ3DXILPeTcbA6Zzqutzlw&s",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo8NcTi9bz0riRYRiqd4K7JPeoan7BjDjXWw&s"
+        ]
+    },
+
+    "demographics": {
+        "websites": [
+            {
+                "title": "Demographics of Bengaluru – Wikipedia",
+                "urls": [
+                    "https://en.wikipedia.org/wiki/Bangalore#Demographics"
+                ]
+            }
+        ],
+        "youtube": [],
+        "images": []
+    },
+
+    "tourism": {
+        "websites": [
+            {
+                "title": "Tourism in Bengaluru – Wikipedia",
+                "urls": [
+                    "https://en.wikipedia.org/wiki/Tourism_in_Bangalore",
+                    "https://bengaluruurban.nic.in/en/tourism/",
+                    "https://www.tripadvisor.in/Attractions-g297628-Activities-Bengaluru_Bangalore_District_Karnataka.html"
+                ]
+            }
+        ],
+        "youtube": [
+            {"title":"Tourism in Bengaluru 1","url":"https://youtu.be/w27ekGRdvuk?si=an9I-iDRU_pLmxOG"},
+            {"title":"Tourism in Bengaluru 2","url":"https://youtu.be/RBmQqhgxjvI?si=MVR5AmCBtOFJY_Ks"},
+            {"title":"Tourism in Bengaluru 3","url":"https://youtu.be/z7cRTSP0vwo?si=OGIqVgpq7iYhzgr-"}
+        ],
+        "images": [
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRppQqMEStr_M1zxkEyc5Tpv0XjfxnsKwguYQ&s",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-P0MSZmMFuyUcAwemC2SCjNUE7D14n81glQ&s",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9EkjWWY80OJ-mFOv50g6a6Zvbud6GcE9jcw&s"
+        ]
+    }
+}
